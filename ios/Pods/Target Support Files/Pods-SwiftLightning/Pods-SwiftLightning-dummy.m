385f86b065799b6f990cf4c87861134d2a0c90ec
Nikolai Heum <nikolaiheum@gmail.com>
2018-07-13 11:19:49 +0200

    .gitignore is now working

--- ios/Pods/Target Support Files/Pods-SwiftLightning/Pods-SwiftLightning-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_SwiftLightning : NSObject
-@end
-@implementation PodsDummy_Pods_SwiftLightning
-@end
--- ios/Pods/Target Support Files/SwiftRangeSlider/SwiftRangeSlider-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_SwiftRangeSlider : NSObject
-@end
-@implementation PodsDummy_SwiftRangeSlider
-@end
